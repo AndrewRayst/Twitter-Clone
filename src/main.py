a5f34882dfe875bd7df6255c346e6dd2fe25eadd
AndrewRayst <mr.darmelow@gmail.com>
2023-10-03 17:03:29 +0400

    feature(src) add loguru for logging
    added loguru

--- src/main.py
+++ src/main.py
@@ -1,6 +1,19 @@
+from datetime import datetime
+
 from fastapi import FastAPI
+from loguru import logger
 
 import config
+
+logger.add(
+    f"../logs/{datetime.now().strftime('%Y-%m-%d')}_log.json",
+    level="INFO",
+    format="{level} {time} {message}",
+    serialize=True,
+    rotation="00:00",
+    compression="zip",
+)
+
 
 application: FastAPI = FastAPI(
     debug=config.DEBUG,
