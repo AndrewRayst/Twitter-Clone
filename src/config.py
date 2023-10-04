89356200837a7906dac1802a6e80f12e7e30e9fe
AndrewRayst <mr.darmelow@gmail.com>
2023-10-04 10:18:05 +0400

    feature(src) add sqlalchemy
    added sqlalchemy
    change Dockerfile for fastAPI server

--- src/config.py
+++ src/config.py
@@ -5,3 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DEBUG: bool = bool(os.getenv("FASTAPI_DEBUG", False))
+
+POSTGRES_USER = os.getenv("POSTGRES_USER", "")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "")
+DB_URL: str = (
+    f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost/{POSTGRES_DB}"
+)
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from loguru import logger
 
 import config
+from database.core import shutdown_db
 
 logger.add(
     f"../logs/{datetime.now().strftime('%Y-%m-%d')}_log.json",
@@ -21,3 +22,8 @@ application: FastAPI = FastAPI(
     version="0.1.0",
     description="Thesis by Andrey Telitsin for Skillbox",
 )
+
+
+@application.on_event("shutdown")
+async def _on_shutdown() -> None:
+    await shutdown_db()
