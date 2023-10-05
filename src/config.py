33e39e0c492b89cfa8da197d9e471dd3686d96dd
AndrewRayst <mr.darmelow@gmail.com>
2023-10-05 15:54:03 +0400

    feature(alembic) add the possibility of async connection for alembic to database
    deleted psycopg2-binary

--- src/config.py
+++ src/config.py
@@ -6,9 +6,9 @@ load_dotenv()
 
 DEBUG: bool = bool(os.getenv("FASTAPI_DEBUG", False))
 
-POSTGRES_USER = os.getenv("POSTGRES_USER", "")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "")
+POSTGRES_USER: str = os.getenv("POSTGRES_USER", "")
+POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "")
+POSTGRES_DB: str = os.getenv("POSTGRES_DB", "")
 DB_URL: str = "postgresql+asyncpg://{}:{}@database:5432/{}".format(
     POSTGRES_USER,
     POSTGRES_PASSWORD,
