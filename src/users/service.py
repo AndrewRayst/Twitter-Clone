52a583b777265c5c5a17980a3531f7f8ba51bcbf
AndrewRayst <mr.darmelow@gmail.com>
2023-10-10 12:07:17 +0400

    feature(users) add endpoints for retrieving your own profile and another user's profile by id

--- src/users/service.py
+++ src/users/service.py
@@ -1,24 +1,80 @@
 from sqlalchemy import Delete, Select, delete, select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
 
 from src.exceptions import ConflictError, ExistError
 from src.users.models import UserFollowerModel, UserModel
 from src.utils import get_hash
 
 
-async def follow_user(
-    session: AsyncSession, user_id: int, follower_api_key: str
-) -> None:
+async def get_me(session: AsyncSession, api_key: str) -> UserModel:
+    """
+    The service for getting your profile
+    :param session: session to connect to the database
+    :param api_key: API key of the user who wants to get their profile
+    :return: user with followers and following
+    """
+    # Getting user
+    query: Select = (
+        select(UserModel)
+        .where(UserModel.api_key_hash == get_hash(api_key))
+        .options(joinedload(UserModel.followers))
+        .options(joinedload(UserModel.following))
+    )
+    user: UserModel = await session.scalar(query)
+
+    # Checking the existence of a user
+    if not user:
+        raise ExistError("The user doesn't exist")
+
+    return user
+
+
+async def get_user(session: AsyncSession, user_id: int, api_key: str) -> UserModel:
+    """
+    The service for getting user profile by id
+    :param session: session to connect to the database
+    :param user_id: id of the user whose profile you want to get
+    :param api_key: api key of the user who wants to retrieve the profile
+    :return: user with followers and following
+    """
+    # Getting user who wants to retrieve the profile
+    visitor_query: Select = select(UserModel).where(
+        UserModel.api_key_hash == get_hash(api_key)
+    )
+    visitor: UserModel = await session.scalar(visitor_query)
+
+    # Checking the existence of the user who wants to retrieve the profile
+    if not visitor:
+        raise ExistError("The user who wants to retrieve the profile doesn't exist")
+
+    # Getting user
+    query: Select = (
+        select(UserModel)
+        .where(UserModel.id == user_id)
+        .options(joinedload(UserModel.followers))
+        .options(joinedload(UserModel.following))
+    )
+    user: UserModel = await session.scalar(query)
+
+    # Checking the existence of a user
+    if not user:
+        raise ExistError("The user doesn't exist")
+
+    return user
+
+
+async def follow_user(session: AsyncSession, user_id: int, api_key: str) -> None:
     """
     The service for following user
     :param session: session to connect to the database
     :param user_id: id of the user to follow
-    :param follower_api_key: API key of the user who wants to follow
+    :param api_key: API key of the user who wants to follow
     :return: None
     """
     # Getting follower
     follower_query: Select = select(UserModel).where(
-        UserModel.api_key_hash == get_hash(follower_api_key)
+        UserModel.api_key_hash == get_hash(api_key)
     )
     follower: UserModel = await session.scalar(follower_query)
 
@@ -47,19 +103,17 @@ async def follow_user(
     await session.commit()
 
 
-async def unfollow_user(
-    session: AsyncSession, user_id: int, follower_api_key: str
-) -> None:
+async def unfollow_user(session: AsyncSession, user_id: int, api_key: str) -> None:
     """
     The service for unfollowing user
     :param session: session to connect to the database
     :param user_id: id of the user to unfollow
-    :param follower_api_key: API key of the user who wants to unfollow
+    :param api_key: API key of the user who wants to unfollow
     :return: None
     """
     # Getting follower
     follower_query: Select = select(UserModel).where(
-        UserModel.api_key_hash == get_hash(follower_api_key)
+        UserModel.api_key_hash == get_hash(api_key)
     )
 
     follower: UserModel = await session.scalar(follower_query)
