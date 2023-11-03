464db596f79a43cf489862feb7970b8678511fb2
Телицин Андрей <mr.darmelow@gmail.com>
2023-11-03 18:17:06 +0300

    Update src/users/router.py

--- src/users/router.py
+++ src/users/router.py
@@ -123,9 +123,7 @@ async def _follow(
         return await return_server_exception(exception=exc)
 
 
-@router.post(
-    "/{user_id}/unfollow", response_model=SuccessResponseSchema, status_code=200
-)
+@router.post("/{user_id}/follow", response_model=SuccessResponseSchema, status_code=200)
 async def _unfollow(
     user_id: int,
     api_key: str = Depends(api_key_param),
